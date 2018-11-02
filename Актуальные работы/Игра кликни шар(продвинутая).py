9da43c0c3628f559006d8f101c155b56dd82ede2
Geelay <gleb.kuz@bk.com>
2018-11-02 10:17:41 +0300

    version 1.0

---
+++ Актуальные работы/Игра кликни шар(продвинутая).py
@@ -0,0 +1,84 @@
+from tkinter import *
+import random
+import graphics as gr
+root = Tk()
+root.geometry("800x600")
+canvas = Canvas(root)
+canvas.pack(fill=BOTH, expand=1)
+schet=0
+speed=100
+number_of_balls=10
+x=[]
+y=[]
+text1=canvas.create_text(500, 25, text='Счёт: '+str(schet),font='Arial 25')
+def cliick(event):
+    global schet,x,y,speed,text1
+    
+    a = event.x
+    b = event.y
+    for i in range (number_of_balls):
+        if a in range(x[i], x[i]+50) and b in range(y[i], y[i]+50):  
+            canvas.delete(ov[i])
+            canvas.delete(text1)
+            text1=canvas.create_text(500, 25, text='Счёт: '+str(schet),font='Arial 25')
+            schet +=1
+            if speed > 10:
+                speed-=10
+        #создание текста со счётом
+            
+def create_list_of_balls(number):
+    lst = []
+    while len(lst) < number:
+        lst.append(random.randint(1,5))
+    return(lst)
+
+
+def risovach(i):
+    global x,y
+    #рандомим переменные red blu gre, чтобы получить цвет
+    red=random.randint(0,255)
+    gre=random.randint(0,255)
+    blu=random.randint(0,255)
+    #рандомим положение круга
+    x.append(random.randint(0,740))
+    y.append(random.randint(0,500))
+    #создаём круг
+    ovall=canvas.create_oval(x[i], y[i], x[i]+50, y[i]+50,fill=gr.color_rgb(red,gre,blu))
+    return (ovall)
+
+def tick_handler():
+    global x, y,balls
+    for i in range (number_of_balls):
+
+        if x[i] < 40:
+            ballsx[i] = -ballsx[i]; x[i] = 40
+        elif x[i] > 740-40:
+            ballsx[i] = -ballsx[i]
+            x[i] = 740-40
+        if y[i] < 40:
+            ballsy[i] = -ballsy[i]
+            y[i] = 40
+        elif y[i] > 500-40:
+            ballsy[i] = -ballsy[i]
+            y[i] = 500-40
+        x[i] = x[i] + ballsx[i]; y[i] = y[i] + ballsy[i]
+        canvas.move(ov[i], ballsx[i], ballsy[i])
+        
+        
+
+
+def time_handler():
+    global speed
+    tick_handler()
+    root.after(speed, time_handler)
+
+
+ov=[]
+for i in range(number_of_balls):
+    bll=risovach(i)
+    ov.append(bll)    
+ballsx=create_list_of_balls(number_of_balls)
+ballsy=create_list_of_balls(number_of_balls)
+root.after(10, time_handler)
+canvas.bind('<Button-1>', cliick)
+root.mainloop()
